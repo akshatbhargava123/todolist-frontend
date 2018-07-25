5be092be28f88d1fe092e8ebadd6d7d4dbc9aeac
akshat <akshatbhargavadeepa@gmail.com>
2018-07-25 08:41:38 +0530

    Connected PluggableTodo component to store

--- src/components/PluggableTodo/PluggableTodo.js
+++ src/components/PluggableTodo/PluggableTodo.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import './PluggableTodo.css';
+import { connect } from 'react-redux';
+
+import addItem from '../../actions/addItem.action';
 
 class PluggableTodo extends Component {
   render() {
+    console.log(this.props)
     return (
       <div className="container">
+        <h1>List Name</h1>
         <p>
           <label htmlFor="new-task">Add Item</label>
           <input id="new-task" type="text" />
@@ -23,7 +27,7 @@ class PluggableTodo extends Component {
           <li className="editMode">
             <input type="checkbox" />
             <label>Go Shopping</label>
-            <input type="text" value="Go Shopping" />
+            <input type="text" value="Go Shopping" onChange={() => console.assert('hi')} />
             <button className="edit">Edit</button>
             <button className="delete">Delete</button>
           </li>
@@ -44,4 +48,16 @@ class PluggableTodo extends Component {
   }
 }
 
-export default PluggableTodo;
+const mapStateToProps = (storeState, props) => {
+  console.log(storeState)
+  return {
+    list: storeState.lists.find(list => list.id === props.listId),
+    ...props
+  };
+}
+
+const mapActionsToProps = {
+  addItem
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(PluggableTodo);
