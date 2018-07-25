e2d1dd8ca44fbf73c9fc2a3c882b9c6ab72d6200
akshat <akshatbhargavadeepa@gmail.com>
2018-07-25 08:41:23 +0530

    defined routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { Route, Redirect, withRouter } from "react-router-dom";
 import './App.css';
+
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Lists from './components/Lists/Lists';
 import PluggableTodo from './components/PluggableTodo/PluggableTodo';
+
+
 
 class App extends Component {
   render() {
+    const { pathname } = this.props.location;
     return (
-      <PluggableTodo listId={1} />
+      <div>
+        {pathname === '/' ? <Redirect to="login" /> : ''}
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <PrivateRoute path="/lists" component={Lists} />
+        <PrivateRoute path="/lists/:id" component={PluggableTodo} />
+      </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
