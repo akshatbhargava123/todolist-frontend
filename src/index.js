eb217191e09d747d0a1036c1e7a5e1756ebe56a0
akshat <akshatbhargavadeepa@gmail.com>
2018-07-25 08:41:04 +0530

    Added react-router-dom package, configured app for it and defined PrivateRoute component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
 import configureStore from './configureStore';
 
 const reduxStore = configureStore();
 
 ReactDOM.render(
   <Provider store={reduxStore}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
